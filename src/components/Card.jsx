a45ca08e443cddda2ff995281db2bf10a5916c7a
Lenny Uwaeme <lenyeuwaeme@gmail.com>
2024-10-09 20:38:03 -0400

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const Card = ({name,title,interest,linkedIn,gitHub}) => {
   return (
     <div className="flex flex-col  w-80 sm:w-64 lg:w-80  gap-2 p-4 rounded-md bg-white shadow-md shadow-slate-300">
-        <img src="src/assets/Placeholder.png" alt="placeholder" className="h-52 w-full rounded aspect-video	"/>
+        <img src="public/Placeholder.png" alt="placeholder" className="h-52 w-full rounded aspect-video	"/>
         <h2 className="text-base font-medium">{name}</h2>
         <p className="text-sm">{title}</p>
         <details>
